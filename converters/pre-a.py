7f19bce9ad46670a9b80f37c951aab10621a2b85
Yuchin Juan <guestwalk@gmail.com>
2016-07-26 17:53:05 -0700

    explain what is target_cat_feats

--- converters/pre-a.py
+++ converters/pre-a.py
@@ -13,6 +13,7 @@ parser.add_argument('dense_path', type=str)
 parser.add_argument('sparse_path', type=str)
 args = vars(parser.parse_args())
 
+#These features are dense enough (they appear in the dataset more than 4 million times), so we include them in GBDT
 target_cat_feats = ['C9-a73ee510', 'C22-', 'C17-e5ba7672', 'C26-', 'C23-32c7478e', 'C6-7e0ccccf', 'C14-b28479f6', 'C19-21ddcdc9', 'C14-07d13a8f', 'C10-3b08e48b', 'C6-fbad5c96', 'C23-3a171ecb', 'C20-b1252a9d', 'C20-5840adea', 'C6-fe6b92e5', 'C20-a458ea53', 'C14-1adce6ef', 'C25-001f3601', 'C22-ad3062eb', 'C17-07c540c4', 'C6-', 'C23-423fab69', 'C17-d4bb7bd8', 'C2-38a947a1', 'C25-e8b83407', 'C9-7cc72ec2']
 
 with open(args['dense_path'], 'w') as f_d, open(args['sparse_path'], 'w') as f_s:
